e32f6b6c511c37bb9395cd0b0d26106e9cfe3bac
fritzmg <github@spikx.net>
2016-01-04 14:43:56 +0100

    added default value for defaultDensities

---
+++ system/modules/default_densities/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   default_densities
+ * @author    Fritz Michael Gschwantner <https://github.com/fritzmg>
+ * @license   GNU/LGPL
+ * @copyright Fritz Michael Gschwantner 2015
+ */
+
+
+$GLOBALS['TL_CONFIG']['defaultDensities'] = '2x';
